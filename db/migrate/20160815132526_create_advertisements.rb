3993b6363ba7e7533f0c920bc58cc8d3bfb1e96d
Miroslav Gojic <miroslavy2k@gmail.com>
2016-08-16 21:36:44 +0200

    Heroku fix #4

--- db/migrate/20160815132526_create_advertisements.rb
+++ db/migrate/20160815132526_create_advertisements.rb
@@ -1,16 +1,15 @@
 class CreateAdvertisements < ActiveRecord::Migration[5.0]
   def change
-    create_table :advertisements do |t|
-		t.string 	:title, null: false, default: ""
-		t.text :description		    	
-		t.text 		:description
-     	t.decimal :price, null: false, precision: 7, scale: 3
-     	t.date 		:year, null: false
-     	t.integer :capacity, null: false
-     	t.integer :power, null: false
+      create_table :advertisements do |t|
+		  t.string 	:title, null: false, default: ""
+		  t.text :description		    	
+   	  t.decimal :price, null: false, precision: 7, scale: 3
+   	  t.date 		:year, null: false
+ 	    t.integer :capacity, null: false
+   	  t.integer :power, null: false
      	t.decimal :mileage, precision: 9, scale: 3
   		  
-        t.timestamps null: false
+      t.timestamps null: false
     end
   end
 end
